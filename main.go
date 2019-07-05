d38fd55950b6190facca7b1b1be1f3d8ab0fcf66
Joao Henrique Machado Silva <joaoh82@gmail.com>
2019-07-05 18:18:06 +0200

    bubble sort algorithm

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ func main() {
 	fmt.Printf("Binary Search Algorithm\n")
 	result := binarySearch([]int{2, 3, 4, 5, 56, 67, 78, 89, 123, 432, 567, 7654, 23456}, 78)
 	fmt.Printf("Result: %v\n", result)
+	data := []int{-2, 45, 0, 11, -9}
+	fmt.Println(data)
+	bubbleSort(data)
+	fmt.Println(data)
 }
 
 // Binary Search is a simple algorithm to find an item in an sorted array.
@@ -30,4 +34,18 @@ func binarySearch(list []int, search int) bool {
 		return false
 	}
 	return true
+}
+
+func bubbleSort(array []int) {
+	size := len(array)
+	for step := 0; step < size-1; step++ {
+		for i := 0; i < size-step-1; i++ {
+			// To sort in descending order, change > to <.
+			if array[i] > array[i+1] {
+				temp := array[i]
+				array[i] = array[i+1]
+				array[i+1] = temp
+			}
+		}
+	}
 }
